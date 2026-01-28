c70a6d40d819b373b9c2144a795f855a87d9d970
Md Esa Alli <mdesaalli980@gmail.com>
2026-01-28 06:44:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4606033305-230d1",
+  "appId": "1:875161418359:web:4960cb52428b62c54ce472",
+  "apiKey": "AIzaSyBsX0YGwmIgFP1Ji_Z3jMAwebacqMaovds",
+  "authDomain": "studio-4606033305-230d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "875161418359"
+};
